989e70239d39abb6ade243da86f38afbd4a223fb
Thunderhacker12 <155258067+Thunderhacker12@users.noreply.github.com>
2023-12-31 00:32:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BRANDED-THUNDER/THUNDER-NEW-MUSIC",
+    "https://github.com/Thunderhacker12/BRANDEDxTHUNDERxMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
