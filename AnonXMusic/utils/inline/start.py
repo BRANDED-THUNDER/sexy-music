770d7e3236555ce8d398195efad453ccae81ece8
Thunderhacker12 <155258067+Thunderhacker12@users.noreply.github.com>
2024-01-01 09:12:57 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -32,6 +32,6 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_7"], "https://t.me/BRANDED_PAID_CC"
-        ],
+        ),
     ]
     return buttons
