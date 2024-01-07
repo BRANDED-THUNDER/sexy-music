1f51d90703fdc9bca594209a658cc82b44ef0301
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2024-01-08 02:28:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6483998374))
+OWNER_ID = int(getenv("OWNER_ID", 6896896077))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -38,14 +38,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Thunderhacker12/BRANDEDxTHUNDERxMUSIC",
+    "https://github.com/BRANDED-THUNDER/sexy-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FAKE_SMILE_76")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FAKE_SMILE_78")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Friends_chatting_Group_Sigma")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
