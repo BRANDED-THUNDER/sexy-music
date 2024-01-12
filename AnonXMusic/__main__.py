e5f4dee5e3f7ba9c1bf7c5df4f09938a85f2b938
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2024-01-12 15:08:07 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Anony.start()
     try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Anony.stream_call("https://te.legra.ph/file/f7308284154ae2454ed6c.jpg")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
